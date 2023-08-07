212a05842ccd2ff44d456d0028715c4a56b40383
ST10084621 <ST10084621@vcconnect.edu.za>
2023-08-07 13:32:32 +0200

    initial commit

---
+++ SGO_V1/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGO_V1.Models;
+using System.Diagnostics;
+
+namespace SGO_V1.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		public IActionResult Team()
+		{
+			return View();
+		}
+
+		public IActionResult Contact()
+		{ 
+			return View();
+		}
+
+		public IActionResult Donate()
+		{
+			return View();
+		}
+
+        public IActionResult Testimonials()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ SGO_V1/Controllers/ServicesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SGO_V1.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Rehab()
+        {
+            return View();
+        }
+
+        public IActionResult MissionarySchool()
+        {
+            return View();
+        }
+
+        public IActionResult OtherServices()
+        { 
+            return View();
+        }
+    }
+}
